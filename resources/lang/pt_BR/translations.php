db2d7b9f957f5544acc8d8a6498a9b6095777896
Thiago França da Silva <contact@tfs.dev.br>
2023-12-31 17:20:03 -0300

    feat(translations): add translation for pt_BR

---
+++ resources/lang/pt_BR/translations.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'breadcrumb' => 'Monitor de Trabalhos em Fila',
+    'title' => 'Trabalhos em Fila',
+    'navigation_label' => 'Trabalhos',
+    'navigation_group' => 'Sistema',
+    'total_jobs' => 'Total de Trabalhos Executados',
+    'execution_time' => 'Tempo consumido',
+    'average_time' => 'Média de Duração',
+    'succeeded' => 'Finalizado com Sucesso',
+    'failed' => 'Falhou',
+    'running' => 'Em Progresso',
+    'status' => 'Estado',
+    'name' => 'Nome',
+    'queue' => 'Fila',
+    'progress' => 'Progresso',
+    'started_at' => 'Iniciado em',
+];
